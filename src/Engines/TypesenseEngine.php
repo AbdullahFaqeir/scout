a2f39cea21c3ded84ec41d4c7fa7714635986b6d
Abdullah Al-Faqeir <abdullah.alfaqeir@gmail.com>
2024-05-03 17:59:53 +0300

    Update TypesenseEngine.php
    Fix variable name.

--- src/Engines/TypesenseEngine.php
+++ src/Engines/TypesenseEngine.php
@@ -492,10 +492,10 @@ class TypesenseEngine extends Engine
      */
     protected function getOrCreateCollectionFromModel($model): TypesenseCollection
     {
-        $index = $this->typesense->getCollections()->{$model->searchableAs()};
+        $collection = $this->typesense->getCollections()->{$model->searchableAs()};
 
-        if ($index->exists() === true) {
-            return $index;
+        if ($collection->exists() === true) {
+            return $collection;
         }
 
         $schema = config('scout.typesense.model-settings.'.get_class($model).'.collection-schema') ?? [];
@@ -510,9 +510,9 @@ class TypesenseEngine extends Engine
 
         $this->typesense->getCollections()->create($schema);
 
-        $index->setExists(true);
+        $collection->setExists(true);
 
-        return $index;
+        return $collection;
     }
 
     /**
